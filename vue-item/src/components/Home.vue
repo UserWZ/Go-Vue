6d8409e4f594a36a94f0a4914df0349e430110db
Allen <13402202685@163.com>
2017-03-08 18:30:43 +0800

    学习vue资料

---
+++ vue-item/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="item-header">
+    <h1>this is home!</h1>
+  </div>
+</template>
+<script>
+    export default {
+        name: 'item-header',
+        data () {
+            return {}
+        }
+    }
+</script>
+<style scoped>
+    * {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+</style>
---
+++ vue-item/src/components/发.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1></h1>
+</template>
+<script>
+    export default {
+        name: '',
+        data () {
+            return {}
+        }
+    }
+</script>
+<style scoped>
+    * {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+</style>
