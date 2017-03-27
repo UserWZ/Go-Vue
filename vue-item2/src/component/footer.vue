01c659819906eb81070ab4b51795764481a45b97
Allen <13402202685@163.com>
2017-03-27 17:25:14 +0800

    add vue-item2

---
+++ vue-item2/src/component/footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="myFooter" class="myFooter">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg:"这是一个尾部"
+        }
+    }
+}
+</script>
+
+<style>
+  .myFooter{
+    width:100%;
+    height:50px;
+    line-height:50px;
+    background-color: darkred;
+  }
+</style>
---
+++ vue-item2/src/component/header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="myHeader" class="myHeader">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+  export default{
+      data() {
+          return {
+              msg : '这是一个头部'
+          }
+      }
+  }
+</script>
+
+<style lang="">
+  .myHeader{
+    width:100%;
+    height:50px;
+    line-height:50px;
+    background-color: green;
+  }
+</style>
