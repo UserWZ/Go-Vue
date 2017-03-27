68a59b3877a51d5ed65eed0aff701371af8ad1ba
Allen <13402202685@163.com>
2017-03-27 15:23:59 +0800

    add vue-item

---
+++ vue-item/src/component/firstcomponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="firstcomponent">
+    <h1>i am a title.</h1>
+    <a>write by {{author}}</a>
+  </div>
+</template>
+
+<script>
+export default{
+    name:'firstcomponent',
+    data(){
+        return {
+            author:"微信公众号 madness-Allen"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
