f3348fb07715ab412222cb25a0811e211172c011
AllenChinese <13402202685@163.com>
2017-04-05 20:58:11 +0800

    修改bug

--- vue-router-cli/src/pages/firstPage.vue
+++ vue-router-cli/src/pages/firstPage.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div>
     <el-upload
       class="upload-demo"
@@ -19,18 +18,6 @@
     </div>
   </div>
 
-=======
-  <el-upload
-    class="upload-demo"
-    action="https://jsonplaceholder.typicode.com/posts/"
-    :on-preview="handlePreview"
-    :on-remove="handleRemove"
-    :file-list="fileList2"
-    list-type="picture">
-    <el-button size="small" type="primary" ref="btn" style="display:block;">点击上传</el-button>
-    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-  </el-upload>
->>>>>>> de6bf7d93628014c17839eef2086734019efd68a
 </template>
   <script>
     export default {
@@ -41,14 +28,6 @@
               {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}
               ]
         };
-      },
-<<<<<<< HEAD
-      mounted (){
-        console.log(this.$root)
-=======
-      mounted() {
-        console.log(this.$refs.btn.style.cssText)
->>>>>>> de6bf7d93628014c17839eef2086734019efd68a
       },
       methods: {
         handleRemove(file, fileList) {
